7fdc3eb3ef5d396ed418bd9dff0161d4d55989e6
Fabien Dumesnil <unctad.fabien.dumesnil@gmail.com>
2016-07-13 15:20:02 +0200

    Added smooth scroll

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
+//	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
 	connect = require('gulp-connect'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
+	minifyhtml = require('gulp-minify-html'),
 	concat = require('gulp-concat');
 
 var env,
+	coffeeSources,
+	jsSources,
 	sassSources,
 	htmlSources,
 	outputDir,
@@ -19,8 +26,22 @@ if (env==='development') {
 	sassStyle ='compressed';
 }
 
+//coffeeSources = ['components/coffee/tagline.coffee'];
+jsSources = [
+//	'components/scripts/rclick.js',
+	'components/scripts/scrollto.js'
+];
 sassSources = ['components/sass/style.scss'];
 htmlSources = [outputDir +'*.html'];
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulpif(env === 'production', uglify({mangle: true, compress: {sequences: true, dead_code: true, conditionals: true, booleans: true, unused: true, if_return: true, join_vars: true, drop_console: true} })))
+		.pipe(gulp.dest(outputDir +'js'))
+		.pipe(connect.reload())
+});
 
 gulp.task('compass', function() {
 	gulp.src(sassSources)
@@ -45,12 +66,13 @@ gulp.task('images', function () {
 });
 
 gulp.task('watch', function() {
+	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 	gulp.watch('builds/development/*.html', ['html']);
 	gulp.watch('builds/development/images/**/*.*', ['images']);
 });
 //gulp.task('all', ['coffee','js','compass']); //chaining gulp tasks
-gulp.task('default', ['html','compass','images','connect','watch']); //gulp task named 'default' will be executed if you just run the gulp command
+gulp.task('default', ['html','js','compass','images','connect','watch']); //gulp task named 'default' will be executed if you just run the gulp command
 
 gulp.task('connect', function() {
 	connect.server({
